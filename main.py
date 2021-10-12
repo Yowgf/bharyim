1f361d50f8c70a34d9cf743f17c1ad2753bc3983
MrMuitoLok0 <heitor983@hotmail.com>
2021-10-12 17:58:34 -0300

    Add Basic Screen

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+import sys
+
+
+screen = pygame.display.set_mode((1080, 720))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            break

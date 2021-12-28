f5ba3ba882afbf7cb0e8a5bfa8ff3689f6c9fa37
jowiebolseiro <loenzomolmassarioli@gmail.com>
2021-12-28 10:39:29 -0300

    Added floor

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ import sys
 
 # Initialize everything
 pygame.init()
-screen = pygame.display.set_mode((500, 500))
+screen_width = 500
+screen_height = 500
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 clock.tick(60)
 
-#variaveis
-player_x = 250
-player_y = 250
+# Initialize floor
+floor_height = 50
+red = (255, 0, 0)
+floor_rectangle = pygame.Rect(0, screen_height - floor_height, screen_width, floor_height)
+
+# Initialize player
 player_size = 40
+player_x = 250
+player_y = screen_height - floor_height - player_size
 
 purple = (128,0,128)
 rectangle = pygame.Rect(player_x, player_y, player_size, player_size)
-
 
 # Create the clock to control the frame rate
 clock = pygame.time.Clock()
@@ -22,11 +28,8 @@ clock = pygame.time.Clock()
 def handle_movement(key):
 	x_movement = 0
 	y_movement = 0
-	step = 1
-	if key[pygame.K_UP] or key[pygame.K_w]:
-		y_movement -= step
-	if key[pygame.K_DOWN] or key[pygame.K_s]:
-		y_movement += step
+	step = 2
+
 	if key[pygame.K_LEFT] or key[pygame.K_a]:
 		x_movement -= step
 	if key[pygame.K_RIGHT] or key[pygame.K_d]:
@@ -52,8 +55,12 @@ while running:
 	# Draw everything
 	# First the background
 	screen.fill((255, 255, 255))
-	# The rectangle
+
+	# TODO: create function `draw` to encapsulate this behavior
+	# Draw everything
+	pygame.draw.rect(screen, red, floor_rectangle)
 	pygame.draw.rect(screen, purple, rectangle)
+
 	# Then flip the screen!
 	pygame.display.flip()
 
